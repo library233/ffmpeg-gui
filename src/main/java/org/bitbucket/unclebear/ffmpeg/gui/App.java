d5fe8403d43741f73c139bfce560a17d7cb6db91
NVXARM <nvxarm@foxmail.com>
2020-02-27 22:42:35 +0800

    add javafx

--- src/main/java/org/bitbucket/unclebear/ffmpeg/gui/App.java
+++ src/main/java/org/bitbucket/unclebear/ffmpeg/gui/App.java
@@ -1,6 +1,19 @@
 package org.bitbucket.unclebear.ffmpeg.gui;
 
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("FFmpeg GUI");
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("app.fxml")), 640, 480));
+        stage.show();
     }
 }
---
+++ src/main/java/org/bitbucket/unclebear/ffmpeg/gui/controller/Main.java
@@ -0,0 +1,4 @@
+package org.bitbucket.unclebear.ffmpeg.gui.controller;
+
+public class Main {
+}
