455c4a8c12620c22b36ea1b7e9d6767cd154e9ce
NVXARM <nvxarm@foxmail.com>
2020-03-02 22:42:01 +0800

    update file chooser in new task popup

--- src/main/java/org/bitbucket/unclebear/ffmpeg/gui/NewTaskPopup.java
+++ src/main/java/org/bitbucket/unclebear/ffmpeg/gui/NewTaskPopup.java
@@ -4,12 +4,18 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.apache.commons.io.FilenameUtils;
 import org.bitbucket.unclebear.ffmpeg.gui.internal.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class NewTaskPopup {
     static final URL FXML = NewTaskPopup.class.getResource("NewTaskPopup.fxml");
@@ -39,5 +45,39 @@ public class NewTaskPopup {
         } else {
             log.info("{} is invalid", task);
         }
+    }
+
+    public void chooseInputFile() throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(root.getScene().getWindow());
+        String path = file.getCanonicalPath();
+        input.setText(path);
+        output.setText(getOutputFile(file).getCanonicalPath());
+    }
+
+    public void chooseOutputFile() throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showSaveDialog(root.getScene().getWindow());
+        if (isSameAsInputFile(file)) {
+            file = getOutputFile(file);
+        }
+        output.setText(file.getCanonicalPath());
+    }
+
+    private File getOutputFile(File inputFile) throws IOException {
+        int i = 0;
+        File outputFile = inputFile;
+        while (outputFile.isFile()) {
+            String path = outputFile.getCanonicalPath();
+            String extension = FilenameUtils.getExtension(path);
+            String string = FilenameUtils.removeExtension(path);
+            outputFile = new File(string + " (" + ++i + ")." + extension);
+        }
+        return outputFile;
+    }
+
+    private boolean isSameAsInputFile(File outputFile) throws IOException {
+        File inputFile = new File(input.getText());
+        return inputFile.isFile() && Files.isSameFile(Path.of(inputFile.toURI()), Path.of(outputFile.toURI()));
     }
 }
