be05ceda2aae97aa89b70c407b59a22c433fdae1
NVXARM <nvxarm@foxmail.com>
2020-03-12 22:26:20 +0800

    add method in event bus

--- src/main/java/org/bitbucket/unclebear/ffmpeg/gui/intermediate/EventBus.java
+++ src/main/java/org/bitbucket/unclebear/ffmpeg/gui/intermediate/EventBus.java
@@ -1,9 +1,16 @@
 package org.bitbucket.unclebear.ffmpeg.gui.intermediate;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
 
 public enum EventBus {
     instance;
+
+    private static Map<String, Queue<Object>> events = new ConcurrentHashMap<>();
+    private static Map<String, List<Callable<Object>>> listeners = new ConcurrentHashMap<>();
 
     public EventBus getInstance() {
         return instance;
@@ -13,5 +20,8 @@ public enum EventBus {
     }
 
     public void subscribe(String channel, Callable<Object> callable) {
+    }
+
+    public void unsubscribe(String channel) {
     }
 }
