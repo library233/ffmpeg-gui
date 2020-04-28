e9c1b82a1336e76168926a45ee8e49c1922b1bbd
NVXARM <nvxarm@foxmail.com>
2020-04-28 11:42:29 +0800

    update quality

--- src/main/java/org/bitbucket/unclebear/ffmpeg/gui/constant/Quality.java
+++ src/main/java/org/bitbucket/unclebear/ffmpeg/gui/constant/Quality.java
@@ -2,10 +2,10 @@ package org.bitbucket.unclebear.ffmpeg.gui.constant;
 
 public enum Quality {
     Q1("Exceptional Quality"),
-    Q2("Acceptable Quality"),
-    Q3("Balanced"),
-    Q4("Small File"),
-    Q5("Tiny File");
+    Q2("Good Quality"),
+    Q3("Acceptable Quality"),
+    Q4("Small File Size"),
+    Q5("Tiny File Size");
 
     private final String description;
 
