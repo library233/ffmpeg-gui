d5b5b2b6e4c6ec29af3acb13a0bf2d1c388903b2
NVXARM <nvxarm@foxmail.com>
2020-05-28 22:15:10 +0800

    get parameters by format and profile

--- src/main/java/org/bitbucket/unclebear/ffmpeg/gui/internal/format/impl/FormatFactory.java
+++ src/main/java/org/bitbucket/unclebear/ffmpeg/gui/internal/format/impl/FormatFactory.java
@@ -1,15 +1,30 @@
 package org.bitbucket.unclebear.ffmpeg.gui.internal.format.impl;
 
+import org.bitbucket.unclebear.ffmpeg.gui.constant.Profile;
 import org.bitbucket.unclebear.ffmpeg.gui.internal.format.Format;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class FormatFactory {
-    public static List<String> parametersPrefix = List.of(
+    private static Map<Class<? extends Format>, Format> formats = Map.of(
+            Mp4Video.class, new Mp4Video(),
+            WebMVideo.class, new WebMVideo(),
+            MkvVideo.class, new MkvVideo(),
+            OggVideo.class, new OggVideo(),
+            Mp4LegacyVideo.class, new Mp4LegacyVideo(),
+            MkvLegacyVideo.class, new MkvLegacyVideo(),
+            AacAudio.class, new AacAudio(),
+            Mp3Audio.class, new Mp3Audio(),
+            OpusAudio.class, new OpusAudio(),
+            OggVorbisAudio.class, new OggVorbisAudio()
+    );
+    public static List<String> parameterPrefix = List.of(
             "-loglevel", "repeat+level+warning",
             "-i"
     );
-    public static List<String> parametersSuffix = List.of(
+    public static List<String> parameterSuffix = List.of(
             "-hide_banner",
             "-map_metadata", "-1",
             "-y"
@@ -19,18 +34,18 @@ public class FormatFactory {
         throw new IllegalAccessException();
     }
 
-    public static List<Format> getAll() {
-        return List.of(
-                new Mp4Video(),
-                new WebMVideo(),
-                new MkvVideo(),
-                new OggVideo(),
-                new Mp4LegacyVideo(),
-                new MkvLegacyVideo(),
-                new AacAudio(),
-                new Mp3Audio(),
-                new OpusAudio(),
-                new OggVorbisAudio()
-        );
+    public static List<String> getParameters(Class<Format> format, Profile profile) {
+        if (!formats.containsKey(format)) {
+            throw new IllegalArgumentException(format.getCanonicalName());
+        }
+        List<String> parameters = formats.get(format).getParameters(profile);
+        if (parameters == null || parameters.isEmpty()) {
+            throw new IllegalArgumentException(String.format("%s (%s)", format.getCanonicalName(), profile.getDescription()));
+        }
+        List<String> result = new ArrayList<>();
+        result.addAll(parameterPrefix);
+        result.addAll(parameters);
+        result.addAll(parameterSuffix);
+        return result;
     }
 }
