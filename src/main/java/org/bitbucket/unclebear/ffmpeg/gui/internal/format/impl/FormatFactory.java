0a22e0c91adf62c6615b030e4f7a8b950f685200
NVXARM <nvxarm@foxmail.com>
2020-05-30 22:52:05 +0800

    refactor

--- src/main/java/org/bitbucket/unclebear/ffmpeg/gui/internal/format/impl/FormatFactory.java
+++ src/main/java/org/bitbucket/unclebear/ffmpeg/gui/internal/format/impl/FormatFactory.java
@@ -4,11 +4,12 @@ import org.bitbucket.unclebear.ffmpeg.gui.constant.Profile;
 import org.bitbucket.unclebear.ffmpeg.gui.internal.format.Format;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 public class FormatFactory {
-    private static Map<Class<? extends Format>, Format> formats = Map.of(
+    private static final Map<Class<? extends Format>, Format> FORMATS = Map.of(
             Mp4Video.class, new Mp4Video(),
             WebMVideo.class, new WebMVideo(),
             MkvVideo.class, new MkvVideo(),
@@ -20,11 +21,11 @@ public class FormatFactory {
             OpusAudio.class, new OpusAudio(),
             OggVorbisAudio.class, new OggVorbisAudio()
     );
-    public static List<String> parameterPrefix = List.of(
+    private static final List<String> PARAMETER_PREFIX = List.of(
             "-loglevel", "repeat+level+warning",
             "-i"
     );
-    public static List<String> parameterSuffix = List.of(
+    private static final List<String> PARAMETER_SUFFIX = List.of(
             "-hide_banner",
             "-map_metadata", "-1",
             "-y"
@@ -34,18 +35,22 @@ public class FormatFactory {
         throw new IllegalAccessException();
     }
 
+    public static Collection<Format> getAll() {
+        return FORMATS.values();
+    }
+
     public static List<String> getParameters(Class<Format> format, Profile profile) {
-        if (!formats.containsKey(format)) {
+        if (!FORMATS.containsKey(format)) {
             throw new IllegalArgumentException(format.getCanonicalName());
         }
-        List<String> parameters = formats.get(format).getParameters(profile);
+        List<String> parameters = FORMATS.get(format).getParameters(profile);
         if (parameters == null || parameters.isEmpty()) {
             throw new IllegalArgumentException(String.format("%s (%s)", format.getCanonicalName(), profile.getDescription()));
         }
         List<String> result = new ArrayList<>();
-        result.addAll(parameterPrefix);
+        result.addAll(PARAMETER_PREFIX);
         result.addAll(parameters);
-        result.addAll(parameterSuffix);
+        result.addAll(PARAMETER_SUFFIX);
         return result;
     }
 }
