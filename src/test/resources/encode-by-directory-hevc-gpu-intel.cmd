61879b330bc820e1e3153b6fc887cf2403176976
NVXARM <nvxarm@foxmail.com>
2021-09-13 08:18:32 +0800

    update

---
+++ src/test/resources/encode-by-directory-hevc-gpu-intel.cmd
@@ -0,0 +1,18 @@
+@echo off
+cd /d %1 && md output && cd || pause && exit /b
+echo.
+for %%f in (*.mp4) do (
+    echo --------------------------------
+    echo %%f
+    ffmpeg                              ^
+    -loglevel warning                   ^
+    -stats                              ^
+    -i "%%f"                            ^
+    -hide_banner                        ^
+    -map_metadata -1                    ^
+    -c:v hevc_qsv                       ^
+    -c:a aac                            ^
+    -n "output\%%f"
+)
+echo.
+pause
