3393637ec0b07c9f0914fa16f491d927ef83aaf2
Chips Burnt <chipsburnt@foxmail.com>
2024-03-07 09:31:09 +0800

    update

--- src/test/resources/encode-by-mpeg4-and-aac.cmd
+++ src/test/resources/encode-by-mpeg4-and-aac.cmd
@@ -1,7 +1,7 @@
 @echo off
 set ffmpeg_output_video_extension=mp4
 set ffmpeg_output_video_encoder=mpeg4
-set ffmpeg_output_audio_extension=aac
+set ffmpeg_output_audio_extension=m4a
 set ffmpeg_output_audio_encoder=aac
 set ffmpeg_output_options=-q:v 3
 bash %~dp0\encode.sh %* | cat
