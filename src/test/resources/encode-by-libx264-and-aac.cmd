ccdaae9b172aa58a20d404295da35412d28cedba
Chips Burnt <chipsburnt@foxmail.com>
2024-03-07 10:29:13 +0800

    update

--- src/test/resources/encode-by-libx264-and-aac.cmd
+++ src/test/resources/encode-by-libx264-and-aac.cmd
@@ -1,7 +1,7 @@
 @echo off
 set ffmpeg_output_video_extension=mp4
 set ffmpeg_output_video_encoder=libx264
-set ffmpeg_output_audio_extension=aac
+set ffmpeg_output_audio_extension=m4a
 set ffmpeg_output_audio_encoder=aac
 bash %~dp0\encode.sh %* | cat
 pause
--- src/test/resources/encode-by-mpeg4-and-aac.cmd
+++ src/test/resources/encode-by-mpeg4-and-aac.cmd
@@ -3,6 +3,6 @@ set ffmpeg_output_video_extension=mp4
 set ffmpeg_output_video_encoder=mpeg4
 set ffmpeg_output_audio_extension=m4a
 set ffmpeg_output_audio_encoder=aac
-set ffmpeg_output_options=-map a:0 -q:v 3
+set ffmpeg_output_options=-q:v 3
 bash %~dp0\encode.sh %* | cat
 pause
